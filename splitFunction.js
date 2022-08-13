d3f77c777c016feaf093d0bac4c05f9fa30bdda3
mhmdy <reza.mhmdy.78@gmail.com>
2022-08-13 12:38:09 +0430

    Array Clutter

--- splitFunction.js
+++ splitFunction.js
@@ -1,20 +1,21 @@
-function generateNumber(count){
+
+function generateNumber(count) {
     let arr=[];
-    for (let i = 0; i <= count; i++) {
-        arr.push(i)
+    for (let counter = 0; counter <= count ; counter++) {
+        arr.push(counter);
     }
     return arr;
 }
 
-// console.log(generateNumber(5));
-
 function splitArray(array) {
-    let lengthArray= array.length;
-    let part  = parseInt(lengthArray/3);
-    let part1 = array.slice(0,part);
-    let part2 = array.slice(part,part*2);
-    let part3 = array.slice(part*2,lengthArray);
+    let length=array.length;
+    let part=parseInt(length/3);
+    let part1=array.slice(0,part);
+    let part2=array.slice(part,part*2);
+    let part3=array.slice(part*2,length);
 
     return [...part2,...part3,...part1];
+    // return [part2,part3,part1];
 }
-console.log(splitArray(generateNumber(9)));
+
+console.log(splitArray(generateNumber(10)));
